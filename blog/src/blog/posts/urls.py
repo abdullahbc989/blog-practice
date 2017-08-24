d15e8411c4bc9233dd6239b0a43d78db823fe330
Abdullah Clute <clute.abdullahb@gmail.com>
2017-08-24 11:51:48 -0500

    Add files via upload

--- blog/src/blog/posts/urls.py
+++ blog/src/blog/posts/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name="home"),
     url(r'^create/$', views.post_create, name='create'),
-    url(r'^(?P<id>\d+)/$', views.post_detail, name='detail'),
-    url(r'^update/$', views.post_update),
-    url(r'^delete/$', views.post_delete),
+    url(r'^(?P<slug>[\w-]+)/$', views.post_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', views.post_update, name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', views.post_delete),
     ]
 
--- blog/src/blog/posts/views.py
+++ blog/src/blog/posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
 from .models import Post
 from .forms import PostForm
 
@@ -15,7 +17,17 @@ def post_list(request):
     #    context = {
     #
     #    }
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()  # .order_by("-
+    paginator = Paginator(queryset_list, 5)
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         "object_list": queryset,
     }
@@ -23,8 +35,8 @@ def post_list(request):
     return render(request, template, context)
 
 
-def post_detail(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     template = "post_detail.html"
     context = {
         "instance": instance
@@ -33,10 +45,14 @@ def post_detail(request, id=None):
 
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "Created!")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, "Error in creation")
 
     template = "post_create.html"
     context = {
@@ -45,9 +61,26 @@ def post_create(request):
     return render(request, template, context)
 
 
-def post_update(request):
-    return HttpResponse("Update")
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, id=id)
+
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Updated!")  # , extra_tags = ''
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    template = "post_create.html"
+    context = {
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, template, context)
 
 
-def post_delete(request):
-    return HttpResponse("Delete")
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    instance.delete()
+    messages.success(request, "Deleted")
+    return redirect("posts:home")
