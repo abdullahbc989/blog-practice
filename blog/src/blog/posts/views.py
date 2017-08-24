80da9c64140c4dd1f3d692efb879bd3c9dbe875b
Abdullah Clute <clute.abdullahb@gmail.com>
2017-08-24 13:44:27 -0500

    Final Commit

--- blog/src/blog/posts/views.py
+++ blog/src/blog/posts/views.py
@@ -1,7 +1,9 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, Http404
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
+from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
@@ -17,7 +19,19 @@ def post_list(request):
     #    context = {
     #
     #    }
-    queryset_list = Post.objects.all()  # .order_by("-
+    today = timezone.now().date()
+    queryset_list = Post.objects.active()  # .order_by("-
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
+
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+        ).distinct()  # no duplicates in search results
     paginator = Paginator(queryset_list, 5)
 
     page = request.GET.get('page')
@@ -30,6 +44,7 @@ def post_list(request):
 
     context = {
         "object_list": queryset,
+        "today": today,
     }
     template = "post_list.html"
     return render(request, template, context)
@@ -37,6 +52,9 @@ def post_list(request):
 
 def post_detail(request, slug=None):
     instance = get_object_or_404(Post, slug=slug)
+    if instance.publish > timezone.now().date() or instance.draft:
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     template = "post_detail.html"
     context = {
         "instance": instance
@@ -45,9 +63,13 @@ def post_detail(request, slug=None):
 
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, "Created!")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -62,7 +84,9 @@ def post_create(request):
 
 
 def post_update(request, slug=None):
-    instance = get_object_or_404(Post, id=id)
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
 
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -80,6 +104,8 @@ def post_update(request, slug=None):
 
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Deleted")
