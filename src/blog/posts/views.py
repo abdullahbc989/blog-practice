ac1c28c59dfdcdfe333ff95f722d2d119a04d00d
Abdullah Clute <Abdullah.Clute@tdstelecom.com>
2017-08-25 11:51:20 -0500

    Added reply feature

--- src/blog/posts/views.py
+++ src/blog/posts/views.py
@@ -1,10 +1,12 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render, get_object_or_404, redirect, Http404
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.utils import timezone
 
+from comments.forms import CommentForm
 from comments.models import Comment
 from .models import Post
 from .forms import PostForm
@@ -57,11 +59,43 @@ def post_detail(request, slug=None):
     if instance.publish > timezone.now().date() or instance.draft:
         if not request.user.is_staff or not request.user.is_superuser:
             raise Http404
+
+    initial_data = {
+        "content_type": instance.get_content_type,
+        "object_id": instance.id,
+    }
+    form = CommentForm(request.POST or None, initial=initial_data)
+    if form.is_valid():
+        c_type = form.cleaned_data.get("content_type")
+        content_type = ContentType.objects.get(model=c_type)
+        obj_id = form.cleaned_data.get("object_id")
+        content_data = form.cleaned_data.get("content")
+        parent_obj = None
+        try:
+            parent_id = int(request.POST.get("parent_id"))
+        except:
+            parent_id = None
+
+        if parent_id:
+            parent_qs = Comment.objects.filter(id=parent_id)
+            if parent_qs.exists() and parent_qs.count() == 1:
+                parent_obj = parent_qs.first()
+
+        new_comment, created = Comment.objects.get_or_create(
+                                user = request.user,
+                                content_type = content_type,
+                                object_id = obj_id,
+                                content = content_data,
+                                parent = parent_obj,
+        )
+        return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
+
     comments = instance.comments  # Comment.objects.filter_by_instance(instance)
     template = "post_detail.html"
     context = {
         "instance": instance,
         "comments": comments,
+        "comment_form": form,
     }
     return render(request, template, context)
 
