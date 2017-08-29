4225122b0e223b48ef871e9b5029928addae0c0e
Abdullah Clute <Abdullah.Clute@tdstelecom.com>
2017-08-29 13:55:36 -0500

    Added pagination feature

--- src/blog/posts/api/views.py
+++ src/blog/posts/api/views.py
@@ -21,6 +21,7 @@ from rest_framework.permissions import (
 
 from posts.models import Post
 
+from .pagination import PostLimitOffsetPagination, PostPageNumberPagination
 from .permissions import IsOwnerOrReadOnly
 from .serializers import (
         PostListSerializer,
@@ -33,6 +34,7 @@ class PostListAPIView(ListAPIView):
     serializer_class = PostListSerializer
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'content', 'user__first_name']
+    pagination_class = PostPageNumberPagination
 
     def get_queryset(self, *args, **kwargs):
         # queryset_list = super(PostListAPIView, self,).get_queryset(*args, **kwargs)
@@ -77,3 +79,4 @@ class PostDeleteAPIView(DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
     lookup_field = 'slug'
+    authentication_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
